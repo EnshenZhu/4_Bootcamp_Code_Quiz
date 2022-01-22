1869e97e1256a675efa497fed840b3894f54c95d
WilsonZhu <winniecmz.zhu@gmail.com>
2022-01-22 13:03:07 -0500

    fix error message

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,61 @@ var questionList=[
     {"number":1,
     "name": "This is the first question",
     "Ans1":"OptionA",
-    "Ans2":"OptionA",
-    "Ans3":"OptionA",
-    "Ans4":"OptionA"},
+    "Ans2":"OptionB",
+    "Ans3":"OptionC",
+    "Ans4":"OptionD"},
 
     {"number":2,
     "name": "This is the second question",
     "Ans1":"OptionA",
-    "Ans2":"OptionA",
-    "Ans3":"OptionA",
-    "Ans4":"OptionA"},
+    "Ans2":"OptionB",
+    "Ans3":"OptionC",
+    "Ans4":"OptionD"},
+
+    {"number":3,
+    "name": "This is the third question",
+    "Ans1":"OptionA",
+    "Ans2":"OptionB",
+    "Ans3":"OptionC",
+    "Ans4":"OptionD"}
 ];
 var questionCounter=0
 
 // define elements in the header
+var timmerEl=document.querySelector(".timmer");
+timmerEl.textContent="60 sec";
 var formEl=document.querySelector(".form-group")
 var startBtnEl=document.querySelector("#quiz-start");
 
 // define elements in the main body
 var pageContentEl=document.querySelector("#page-content");
-var allQuestionEl=document.querySelector("#question-sheet");
+var allQuestionEl=document.querySelector(".question-sheet");
 var questionTitleEl=document.querySelector(".question-title");
+
+var normalQuestion=function(){
+    questionTitleEl.textContent
+    ="Question "+questionList[questionCounter].number+": "+questionList[questionCounter].name
+
+    var allOptionEl=document.createElement('ul')
+    
+    allOptionEl.className="question-options";
+    allOptionEl.innerHTML=
+    "<input type='radio' value='A'/>"+questionList[questionCounter].Ans1+"</br>"
+    +"<input type='radio' value='B'/>"+questionList[questionCounter].Ans2+"</br>"
+    +"<input type='radio' value='B'/>"+questionList[questionCounter].Ans3+"</br>"
+    +"<input type='radio' value='B'/>"+questionList[questionCounter].Ans4+"</br>"
+    allQuestionEl.appendChild(allOptionEl);
+
+    // add [next] button
+    var nextBtnEl=document.createElement('button')
+    nextBtnEl.className="btn"
+    nextBtnEl.setAttribute("id","quiz-next")
+    nextBtnEl.textContent="Next"
+
+    allQuestionEl.appendChild(nextBtnEl)
+
+    questionCounter++;
+}
 
 var startQuiz=function(){
     // event.preventDefault();
@@ -37,14 +71,13 @@ var startQuiz=function(){
     // start the timmer
 
     // generate normal questions
-    questionTitleEl.textContent
-    ="Question "+questionList[questionCounter].number+": "+questionList[questionCounter].name
+    if (questionCounter<length(questionList)-1){
+        normalQuestion();
+    }
+    
+    
 
-    var allOptionEl=document.createElement('ul')
-    allOptionEl.className="question-options"
-
-    var singleOptionEl=document.createElement('ol')
-    singleOptionEl.className="single=option"
+    // debugger;
 
     // generate last question
 };
